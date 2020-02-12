9146ab64038ef5d61093856be88c09b31818fbe0
S00188447 <47103333+S00188447@users.noreply.github.com>
2020-02-12 11:21:03 +0000

    Labsheet 2 started

---
+++ LabSheet2/Ex1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        private static void NumbersLambda()
+        {
+
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void NumbersQuery()
+        {
+
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers =
+                from number in numbers
+                where number > 5
+                orderby number
+                select number;
+
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Labsheet1/LabSheer2/Ex1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheer2
+{
+    class Ex1
+    {
+        private static void NumbersLambda()
+        {
+
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach(int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void NumbersQuery()
+        {
+
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers =
+                from number in numbers
+                where number > 5
+                orderby number
+                select number;
+
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+
+   
+}
