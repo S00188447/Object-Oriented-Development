7fc7e00a4541d09e7463f05c3c8765b28225dd13
S00188447 <47103333+S00188447@users.noreply.github.com>
2020-03-03 13:00:40 +0000

    Labsheet4 started

---
+++ LabSheet4/LabSheet4/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabSheet4
+{
+
+    public partial class MainWindow : Window
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //lbxStock.ItemsSource = Enum.GetNames(typeof(StockLevel));
+
+            var query1 = from s in db.Suppliers
+                         orderby s.CompanyName
+                         select new
+                         {
+                             SupplierName = s.CompanyName,
+                             SupplierID = s.SupplierID,
+                             Country = s.Country
+                         };
+
+            
+
+            lbxSuppliers.ItemsSource = query1.ToList();
+
+            var query2 = query1
+            .OrderBy(s => s.Country)
+            .Select
+
+        }
+
+        private void lbxStock_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
