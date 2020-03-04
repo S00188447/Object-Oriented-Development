9a5a27f9221e77445d5bb89e733cb5a1571d01d1
S00188447 <47103333+S00188447@users.noreply.github.com>
2020-03-04 20:25:32 +0000

    Excercise 1 of labsheet 4 completed

--- LabSheet4/LabSheet4/MainWindow.xaml.cs
+++ LabSheet4/LabSheet4/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 
 namespace LabSheet4
 {
-
+    public enum StockLevel {Low,Normal, Overstocked }
     public partial class MainWindow : Window
     {
         NORTHWNDEntities db = new NORTHWNDEntities();
@@ -27,7 +27,7 @@ namespace LabSheet4
 
         private void window_Loaded(object sender, RoutedEventArgs e)
         {
-            //lbxStock.ItemsSource = Enum.GetNames(typeof(StockLevel));
+            lbxStock.ItemsSource = Enum.GetNames(typeof(StockLevel));
 
             var query1 = from s in db.Suppliers
                          orderby s.CompanyName
@@ -42,15 +42,87 @@ namespace LabSheet4
 
             lbxSuppliers.ItemsSource = query1.ToList();
 
+            //Populate the countries list
+            //var query2 = from s in db.Suppliers
+            //             orderby s.Country
+            //             select s.Country;
+
+            //easier to base this on query 1 which has the info
             var query2 = query1
             .OrderBy(s => s.Country)
-            .Select
+            .Select(s => s.Country);
 
+            var countries = query2.ToList();
+            lbxCountries.ItemsSource = countries.Distinct();
         }
 
         private void lbxStock_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+            //get stock level selected
+
+            var query = from p in db.Products
+                        where p.UnitsInStock < 50
+                        orderby p.ProductName
+                        select p.ProductName;
+
+            string selected = lbxStock.SelectedItem as string;
+
+            switch(selected)
+            {
+                case "Low":
+                    //do nothing as query sorted from above
+                    break;
+
+                case "Normal":
+                    query = from p in db.Products
+                            where p.UnitsInStock >= 50 && p.UnitsInStock <= 100
+                            orderby p.ProductName
+                            select p.ProductName;
+
+                    //select p;
+                    break;
+                case "Overstocked":
+                    query = from p in db.Products
+                            where p.UnitsInStock > 100
+                            orderby p.ProductName
+                            select p.ProductName;
+                    //select p;
+                    break;
+            }
+
+            lbxProducts.ItemsSource = query.ToList();
+
+
+        }
+
+        private void lbxSuppliers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //using the selected value path
+            int supplierID = Convert.ToInt32(lbxSuppliers.SelectedValue);
+
+            //messagebox.show(supplierID.ToString());
+
+            var query = from p in db.Products
+                        where p.SupplierID == supplierID
+                        orderby p.ProductName
+                        select p.ProductName;
+
+            //update product list
+            lbxProducts.ItemsSource = query.ToList();
+        }
+
+         private void lbxCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string country = (string)(lbxCountries.SelectedValue);
+
+            var query = from p in db.Products
+                        where p.Supplier.Country == country
+                        orderby p.ProductName
+                        select p.ProductName;
+
+            //update product list
+            lbxProducts.ItemsSource = query.ToList();
         }
     }
 }
