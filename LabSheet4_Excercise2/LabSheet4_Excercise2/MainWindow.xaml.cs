c0e1f68ed2df014b401237a119ab1ff9c3a59d2a
S00188447 <47103333+S00188447@users.noreply.github.com>
2020-03-04 21:02:15 +0000

    Excercise 2 Completed

---
+++ LabSheet4_Excercise2/LabSheet4_Excercise2/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabSheet4_Excercise2
+{
+
+    public partial class MainWindow : Window
+    {
+        
+        AdventureLiteEntities db = new AdventureLiteEntities();
+        public MainWindow()
+        {
+           
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from o in db.SalesOrderHeaders
+                        orderby o.Customer.CompanyName
+                        select o.Customer.CompanyName;
+
+            //lbxCustomers.ItemsSource = query.ToList().Distinct();         
+        }
+
+        private void lbxCustomers_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+            //string customer = lbxCustomers_SelectionChanged.SelectedItem as string;
+            string customer = "";
+
+            if(customer != null)
+            {
+                //var query = from o in db.SalesOrderHeaders
+                //            where o.Customer.CompanyName.Equals(customer)
+                //            select new OrderSummary
+                //            {
+                //                SalesOrderID = o.SalesOrderID,
+                //                OrderDate = o.OrderDate,
+                //                TotalDue = o.TotalDue
+                //            };
+
+                //var query from o in db.SalesOrderHeaders
+                //          where o.Customer.CompanyName.Equals(customer)
+                //          select o;
+
+                //lbxOrders.ItemSource = query.ToList();
+            }
+
+
+    
+        }
+
+        private void lbxOrders_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+            //int orderID = Convert.ToInt32(lbxOrder.SelectedValue);
+            int orderID = 1;
+
+            if (orderID > 0)
+            {
+                var query = from od in db.SalesOrderDetails
+                            where od.SalesOrderID == orderID
+                            select new
+                            {
+                                ProductName = od.Product.Name,
+                                od.UnitPrice,
+                                od.UnitPriceDiscount,
+                                od.OrderQty,
+                                od.LineTotal
+                            };
+
+                //dgOrderDetails.ItemsSource = query.ToList();
+            }
+     
+        }
+
+
+    }
+}
---
+++ LabSheet4_Excercise2/LabSheet4_Excercise2/SalesOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet4_Excercise2
+{
+    class SalesOrder
+    {
+
+        //public override string ToString()
+        //{
+        //    return string.Format("{0}:{1} {2:C}",
+        //    OrderDate.ToShortDateString(),
+        //    SalesOrderID,
+        //    TotalDue);
+        //}
+    }
+}
